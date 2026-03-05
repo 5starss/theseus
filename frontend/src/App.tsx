4e0f1c4f25d9c22a9c99c95c11e1c20e33606fff
조원혁 <ccc2900@naver.com>
2026-03-05 13:47:28 +0900

    S14P21A503-64 [feat]: 회원가입 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "@/components/layout/Layout.tsx";
 import Home from "./pages/Home";
-import LoginPage from "./pages/LoginPage.tsx";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
