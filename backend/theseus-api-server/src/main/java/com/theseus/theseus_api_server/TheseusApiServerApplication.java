c3e85e17c19231fbcd2cb03d18c5010f7f1eaec3
손석우 <djfzmsdl95@gmail.com>
2026-04-23 16:56:39 +0900

    S14P31A308-17 [chore]: BE 스프링 초기 세팅

---
+++ backend/theseus-api-server/src/main/java/com/theseus/theseus_api_server/TheseusApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.theseus.theseus_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheseusApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheseusApiServerApplication.class, args);
+	}
+
+}
